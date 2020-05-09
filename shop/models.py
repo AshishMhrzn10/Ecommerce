b2815c6e242b14bcb583bfd048d1a793ab720b0e
Ashish Maharjan <amaharjan1032@gmail.com>
2020-05-09 17:59:18 +0545

    Second commit

--- shop/models.py
+++ shop/models.py
@@ -9,10 +9,20 @@ class Product(models.Model):
     category = models.CharField(max_length=50, default="")
     subcategory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
-    desc = models.CharField(max_length=300)
+    desc = models.CharField(max_length=3000)
     pub_date = models.DateField()
     image = models.ImageField(upload_to="shop/images", default="")
 
     def __str__(self):
         return self.product_name
 
+
+class Contact(models.Model):
+    msg_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=70, default="")
+    phone = models.CharField(max_length=70, default="")
+    desc = models.CharField(max_length=500, default=0)
+
+    def __str__(self):
+        return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Contact
 from math import ceil
 
 
@@ -27,6 +27,14 @@ def about(request):
 
 
 def contact(request):
+    if request.method == "POST":
+        print(request)
+        name = request.POST.get('name','')
+        email = request.POST.get('email','')
+        phone = request.POST.get('phone','')
+        desc = request.POST.get('desc','')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
     return render(request, 'shop/contact.html')
 
 
@@ -38,8 +46,10 @@ def search(request):
     return render(request, 'shop/search.html')
 
 
-def productView(request):
-    return render(request, 'shop/productView.html')
+def productView(request, myid):
+    #Fetch the product using id
+    product = Product.objects.filter(id=myid)
+    return render(request, 'shop/productView.html',{'product': product[0]})
 
 
 def checkout(request):
