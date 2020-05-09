1fbb61213f9c9505c79a64cdcaca90434be19802
Ashish Maharjan <amaharjan1032@gmail.com>
2020-05-09 10:55:08 +0545

    First Commit

---
+++ shop/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
+
+def index(request):
+    #products =Product.objects.all()
+   # n = len(products)
+    #nSlides = n//4 + ceil((n/4)-(n//4))
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1,nSlides), nSlides])
+    #allProds = [[products,range(1, nSlides), nSlides],[products,range(1, nSlides), nSlides]]
+    params = {'allProds': allProds}
+    #params = { 'no_of_slides': nSlides, 'product': products,'range':range(1,nSlides)}
+    return render(request, 'shop/index.html', params)
+
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+
+def contact(request):
+    return render(request, 'shop/contact.html')
+
+
+def tracker(request):
+    return render(request, 'shop/tracker.html')
+
+
+def search(request):
+    return render(request, 'shop/search.html')
+
+
+def productView(request):
+    return render(request, 'shop/productView.html')
+
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')
